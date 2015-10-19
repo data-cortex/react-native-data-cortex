f1c0f4a85d8ec4b0e08e99b55955b4f1a8c2e323
Yanko Bolanos <y@rem7.com>
2015-10-19 09:08:56 -0700

    Initial Commit

---
+++ RCTDataCortex/RCTDataCortex.h
@@ -0,0 +1,13 @@
+//
+//  RCTDataCortex.h
+//  RCTDataCortex
+//
+//  Created by Yanko Bolanos on 10/19/15.
+//  Copyright © 2015 data-cortex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTDataCortex : NSObject
+
+@end
---
+++ RCTDataCortex/RCTDataCortex.m
@@ -0,0 +1,13 @@
+//
+//  RCTDataCortex.m
+//  RCTDataCortex
+//
+//  Created by Yanko Bolanos on 10/19/15.
+//  Copyright © 2015 data-cortex. All rights reserved.
+//
+
+#import "RCTDataCortex.h"
+
+@implementation RCTDataCortex
+
+@end
