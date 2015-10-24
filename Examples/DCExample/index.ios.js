fb4b3c7b8e04591daf8ed3346d27157967af587d
Yanko Bolanos <y@rem7.com>
2015-10-24 13:32:16 -0700

    added new example

---
+++ Examples/DCExample/index.ios.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  View,
+} = React;
+
+var DataCortex = require('react-native-data-cortex');
+
+var DCExample = React.createClass({
+
+  componentDidMount: function() {
+    DataCortex.init("dYlBxjMTYkXadqhnOyHnjo7iGb5bW1y0", "rs_example");
+    DataCortex.eventWithProperties({"kingdome": "application_launch"});
+    console.log('component mounted');
+  },
+
+  buttonOneClick: function() {
+    console.log('buttonOneClick');
+    DataCortex.eventWithProperties({
+      "kingdome": "user_interaction",
+      "phylum": "buttonOneClick",
+    });
+  },
+
+  buttonTwoClick: function() {
+    console.log('buttonTwoClick');
+    DataCortex.eventWithProperties({
+      "kingdome": "user_purchase",
+      "phylum": "buttonTwoClick",
+    }, "USD", 123.0);
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native Data Cortex Example!
+        </Text>
+        <Text style={styles.instructions}>
+          To send a generic event click the button bellow
+        </Text>
+
+        <TouchableHighlight onPress={this.buttonOneClick.bind(this)}>
+          <Text style={styles.buttonOne}>
+            Event!
+          </Text>
+        </TouchableHighlight>
+
+        <Text style={styles.instructions}>
+          To send an economy event 
+          push the second button!
+        </Text>
+        <TouchableHighlight onPress={this.buttonTwoClick.bind(this)}>
+          <Text style={styles.buttonOne}>
+            EconomyEvent!
+          </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    padding: 30,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  buttonOne: {
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: 'lightblue',
+  },
+});
+
+AppRegistry.registerComponent('DCExample', () => DCExample);
