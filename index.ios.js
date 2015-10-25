fffdd3de3eecb28430a9778672fa5d8dbe7c5390
Yanko Bolanos <y@rem7.com>
2015-10-24 17:44:02 -0700

    added properties check

--- index.ios.js
+++ index.ios.js
@@ -24,10 +24,29 @@ function addUserTag(userTag)
 
 function event(properties)
 {
+    if( properties === null || typeof properties !== 'object' )
+    {
+        throw new Error('properties must be an object');
+    }
+    
 	DataCortex.eventWithProperties(properties);
 }
 
-function economyEvent(properties, spendCurrency, spendAmount)
+function economyEvent(properties)
 {
-	DataCortex.economyWithProperties(properties, spendCurrency, spendAmount);
+
+    if( properties === null || typeof properties !== 'object' )
+    {
+        throw new Error('properties must be an object');
+    }
+
+    if( properties.spendCurrency && typeof properties.spendAmount === 'number' )
+    {
+		DataCortex.economyWithProperties(properties, properties.spendCurrency, properties.spendAmount);
+    }
+    else
+    {
+        throw new Error('You must pass spendCurrency and spendAmount to economyEvent');
+    }
+
 }
